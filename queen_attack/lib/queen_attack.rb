a560debfdc8a9d237a012c0cf1186a64f6bf7301
Vaughn Dice & Janice Laset-Parkerson <student@epicodus.com>
2015-08-03 15:29:31 -0700

    queen attack exercise finished

--- queen_attack/lib/queen_attack.rb
+++ queen_attack/lib/queen_attack.rb
@@ -1,5 +1,10 @@
 class Array
   define_method(:queen_attack?) do |piece_position|
-    false
+    horizontally_in_line = self[1] == piece_position[1]
+    vertically_in_line = self[0] == piece_position[0]
+    diagonally_in_line = (self[0] - piece_position[0]).abs ==
+                         (self[1] - piece_position[1]).abs
+
+    horizontally_in_line || vertically_in_line || diagonally_in_line
   end
 end
--- queen_attack/spec/queen_attack_spec.rb
+++ queen_attack/spec/queen_attack_spec.rb
@@ -1,9 +1,24 @@
 require('rspec')
 require('queen_attack')
 
+#fixed input options assumed
+
 describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally, vertically, or
       diagonally in line with each other') do
     expect([1,1].queen_attack?([2, 3])).to(eq(false))
   end
+
+  it('is true if the coordinates are horizontally in line') do
+    expect([1,1].queen_attack?([2,1])).to(eq(true))
+  end
+
+  it('is true if the coordinates are vertically in line') do
+    expect([1,1].queen_attack?([1,3])).to(eq(true))
+  end
+
+  it('is true if the coordinates are diagonally in line') do
+    expect([1,1].queen_attack?([2,2])).to(eq(true))
+  end
+
 end
